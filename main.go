3cfb5c867a294f1480ba702caf58a446f9a7553e
DhirajToor <121428858+DhirajToor@users.noreply.github.com>
2023-11-23 18:56:53 -0500

    add the code of fetchExchangeRate in main.go

--- main.go
+++ main.go
@@ -6,7 +6,32 @@ import (
 )
 
 func fetchExchangeRates(apiKey string) (ExchangeRates, error) {
+assets := []string{"BTC", "ETH", "XRP"}
+	rates := make(map[string]float64)
 
+	for _, asset := range assets {
+		url := fmt.Sprintf(coinAPIBaseURL, asset, apiKey)
+		resp, err := http.Get(url)
+		if err != nil {
+			return ExchangeRates{}, err
+		}
+		defer resp.Body.Close()
+
+		var data map[string]interface{}
+		if err := json.NewDecoder(resp.Body).Decode(&data); err != nil {
+			return ExchangeRates{}, err
+		}
+
+		// Assuming the API response has a 'rate' field
+		rate, ok := data["rate"].(float64)
+		if !ok {
+			return ExchangeRates{}, fmt.Errorf("unexpected response format for asset %s", asset)
+		}
+
+		rates[asset] = rate
+	}
+
+	return ExchangeRates{Rates: rates}, nil
 }
 
 func getPriceHandler(w http.ResponseWriter, r *http.Request) {
